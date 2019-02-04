ad46496bf15f14f6ac22affc9f86feff302e145c
Jack Kenyon <jackenyon@gmail.com>
2019-02-04 14:53:37 -0500

    DNS Fix:
    DNS fix script added

---
+++ Batch/set_FIT_dns.bat
@@ -0,0 +1,23 @@
+:: Set primary and alternate DNS for IPv4 on Windows Server 2000/2003/2008 & 
+:: Windows XP/Vista/7
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+SET adapterName=
+
+FOR /F "tokens=* delims=:" %%a IN ('IPCONFIG ^| FIND /I "ETHERNET ADAPTER"') DO (
+SET adapterName=%%a
+
+REM Removes "Ethernet adapter" from the front of the adapter name
+SET adapterName=!adapterName:~17!
+
+REM Removes the colon from the end of the adapter name
+SET adapterName=!adapterName:~0,-1!
+
+netsh interface ipv4 set dns name="!adapterName!" static 172.28.2.252 primary
+netsh interface ipv4 add dns name="!adapterName!" 172.28.1.10 index=2
+)
+
+ipconfig /flushdns
+
+:EOF
